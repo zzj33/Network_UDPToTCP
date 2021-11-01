a468528cd103b2028ace66e54e16309b32bb78dc
zzj33 <1204740586@qq.com>
2021-10-31 23:15:50 -0500

    Slow start, not finished

--- src/sender_main.c
+++ src/sender_main.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -19,6 +20,8 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
+#include <time.h>
+#include <sys/queue.h>
 #include "header.h"
 
 
@@ -26,9 +29,9 @@
 #define HEADER_SEQ 1 //1-4 for seq#
 #define HEADER_ACK 5 //5-8 for ack#
 #define HEADER_DATA 9 //4- for data fragment
-#define PCK_SIZE 4096 // TODO: 1472 payload
 #define INITIAL_TIMEOUT 1000 // in ms
 #define INITIAL_SST 64
+#define MAX_BUF_SIZE 1024
 
 /*
 Invariants:
@@ -39,15 +42,34 @@ struct sockaddr_in si_other;
 int s, slen;
 char * buffer; // heap memory, store the whole packet: including header and actual data. Same in receiver
 header_t * header;
+header_t * header_recv; // the header received from recieiver
+int seqNum = 0; //total packet number, start from 0
+int dataSize = PACKET_SIZE - sizeof(header_t);
+int read_start = 0; //fp's index
+char send_buf[MAX_BUF_SIZE][PACKET_SIZE]; //pointer to packets
+
 float cw_size = 1; // congestion window size
-int file_position = 0; // should times the data size to get the byte position
-int base = 0; // the first index in the congestion window
-int tail = 0; // the last index in the congestion window
+float sst = INITIAL_SST;
+int base = 0; // the first index in the congestion window (packet)
+int tail = 0; // the last index in the congestion window (packet)
+int preTail = -1; // tail before recv the new ack; to find new package needed to send
 int dupack = 0;
-int sst = INITIAL_SST;
-int last_ack = -1;
-bool timeout = true;
-int bytes_to_send; // global var, bytes remain to be send
+int bytes_to_send; // global var, total bytes to be send
+int bytes_rem; //bytes remaining to send
+time_que *timeQ; //each packet's timestamp
+struct timespec ts;
+_Bool time_flag = false; //whether timeout
+_Bool sendFin = false; //
+long timeout = 500000; //timeout bound
+
+typedef struct time_que                             //queue
+{
+    STAILQ_ENTRY(time_que) field;
+    long nsec;
+} time_que;
+
+typedef STAILQ_HEAD(que_head, time_que) que_head;    //queue head
+
 
 
 void diep(char *s) {
@@ -59,10 +81,10 @@ void first_handshake(int sockfd, const struct sockaddr_in dest_addr){
     header = calloc(1, sizeof(header_t));
     header->syn = 1;
     header->fin = 0;
-    header->seq = rand()%256; // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c
+    header->seq = seqNum; // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c
     header->ack = 0;
     fprintf(stderr, "ready to send with syn number: %d\n", header->seq);
-    ssize_t bytes_sent = sendto(sockfd, header, sizeof(header), 0, (const struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    ssize_t bytes_sent = sendto(sockfd, header, sizeof(header_t), 0, (const struct sockaddr *)&dest_addr, sizeof(dest_addr));
     // ssize_t bytes_sent = sendto(sockfd, header, sizeof(header), 0, NULL, 0);
     if (bytes_sent == -1){
         diep("Send first-way Handshake");
@@ -70,11 +92,77 @@ void first_handshake(int sockfd, const struct sockaddr_in dest_addr){
 
 }
 
-void slow_start(int sockfd, const struct sockaddr_in dest_addr, FILE* fp){
-    
-    
-    while (!timeout && dupack <3 && cw_size < sst & bytes_to_send > 0){
+//reload the buffer when cw tail reach the send_buf's tail
+//(move the last cw back to the start of buffer)
+void load_buffer(FILE* fp) {
+    read_start += base * dataSize; //move the file index
+    seqNum = read_start / dataSize; //set seqNum back to first load packet
+    preTail -= base;
+    tail -= base; //re-set the base and tail, with the same cw size
+    base = 0;
+    fseek(fp, read_start, SEEK_SET);
+    int i = 0;
+    //packet data into buffer
+    for (; i < MAX_BUF_SIZE & (seqNum+1) * dataSize < bytes_to_send; i++) {
+        header = (header_t *)send_buf[i];
+        header->syn = 0;
+        header->fin = 0;
+        header->seq = seqNum;
+        char* data = send_buf[i] + sizeof(header_t);
+        fread(data, dataSize, 1, fp);
+        seqNum++;
+    }
+    //load the last packet
+    if ((seqNum+1) * dataSize >= bytes_to_send & i < MAX_BUF_SIZE) {
+        header = (header_t *)send_buf[i];
+        header->syn = 0;
+        header->fin = 0;
+        header->seq = seqNum;
+        char* data = send_buf[i] + sizeof(header_t);
+        fread(data, bytes_to_send - (seqNum * dataSize), 1, fp);
+    }
+}
 
+
+void slow_start(int sockfd, const struct sockaddr_in dest_addr, FILE* fp){
+    socklen_t len = sizeof(dest_addr);
+
+    //the first buffer 
+    sst = cw_size / 2;
+    tail = base;
+    cw_size = 1;
+    dupack = 0;
+    time_que *elm;
+    //send data if recv ack, and check timeout, not finished
+    while (!time_flag && dupack < 3 && cw_size < sst & bytes_rem > 0){
+        while (preTail < tail) {
+            //record the timestamp and send the packet
+            clock_gettime(CLOCK_REALTIME, &ts);
+            elm = malloc(sizeof(time_que));
+            elm -> nsec = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+            STAILQ_INSERT_TAIL(timeQ, elm, field);
+            sendto(sockfd, send_buf[preTail+1], PACKET_SIZE, 0, (const struct sockaddr *)&dest_addr, sizeof(dest_addr));
+            preTail++;
+        }
+        //check time out and recv ack
+        clock_gettime(CLOCK_REALTIME, &ts);
+        long curTime = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+        if ((curTime - STAILQ_FIRST(timeQ)->nsec) <= timeout) {
+            int bytes_recv = recvfrom(sockfd, header_recv, sizeof(header_t), MSG_WAITALL, ( struct sockaddr *) &dest_addr, &len);
+            if (bytes_recv != 0) {
+                if (not dup and ackSeq >= base) {
+                    cw += ackSeq - base + 1;
+                    base = ackSeq + 1;
+                    tail = base + cw - 1;
+                    if (tail >= MAX_BUF_SIZE)
+                        load_buffer(fp);
+                } else {
+                    dupack++;
+                }
+            }
+        } else {
+            time_flag = true;
+        }
     }
 
 }
@@ -89,18 +177,12 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
     }
     fseek(fp, 0, SEEK_END);
     int bytes_to_send = ftell(fp);
-    rewind();
+    rewind(fp);
     if (bytesToTransfer < bytes_to_send){
         bytes_to_send = bytesToTransfer;
     } 
     printf("%llu", bytesToTransfer);
-    // unsigned long int num_pck = (bytesToTransfer - 1) / (PCK_SIZE - HEADER_DATA) + 1; //num of packets need to send
     
-
-    
-
-
-
 
 	/* Determine how many bytes to transfer */
 
@@ -118,14 +200,20 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
     }
 
 
-	/* Send data and receive acknowledgements on s*/
-    buffer = calloc(1, PACKET_SIZE);
-    header = (header_t *)buffer;
+	// /* Send data and receive acknowledgements on s*/
+    // buffer = calloc(1, PACKET_SIZE);
+    // header = (header_t *)buffer;
 
     first_handshake(s, si_other);
 
+    timeQ = malloc(sizeof(time_que));
+    STAILQ_INIT(timeQ);
+    
+
+    if (seqNum == 0) load_buffer(fp);
+
     while (bytes_to_send > 0){
-        if (timeout){
+        if (time_flag){
             slow_start(s, si_other, fp);
         }else if (dupack == 3){
             // TODO: fast_recovery()
--- src/test.c
+++ src/test.c
@@ -1,52 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <string.h>
-#include <sys/time.h>
-// int main(){
-// 	unsigned int a = 3234567890; 
-//     printf("%u\n", sizeof(a));
-// 	unsigned char pBuf[4];
-// 	int b=0;
- 
-// 	//int2byte
-//     pBuf[0] =(unsigned char)(a & 0xff);
-// 	pBuf[1] = (unsigned char)((a >> 8) & 0xff);
-//     pBuf[2] = (unsigned char)((a >> 16) & 0xff);
-//     pBuf[3] = (unsigned char)((a >> 24) & 0xff);
- 
-// 	printf("bruce int2byte  ==>> pBuf[0]=%d,pBuf[1]=%d\n",pBuf[0],pBuf[1]);
- 
-//     //byte2int
-// 	b =(unsigned int)( pBuf[0] | (pBuf[1] << 8) | (pBuf[2] << 16) | (pBuf[3] << 24));
-// 	printf("bruce byte2int  ==>> b=%u\n",b);
- 
- 
-// 	return 0;
-// }
+#include <stdint.h>
+#include <sys/queue.h>
 
-void swap(int *x, int *y);
-int main() {  
-    int a = 10, b = 20;
-  
-    swap(&a, &b);
-    printf("a=%d\nb=%d\n", a, b);
- 
+typedef struct num_node                             /* 链表节点结构体 */
+{
+    SLIST_ENTRY(num_node) field;
+    uint32_t num;
+} num_node;
+
+typedef SLIST_HEAD(num_head, num_node) num_head;    /* 链表头结构体 */
+
+/* 合并两个链表 */
+static num_node *merge(num_node *node1, num_node *node2)
+{
+    if (node1 == NULL) {return node2;}
+    if (node2 == NULL) {return node1;}
+
+    num_node *res, *ptr;
+
+    if(node1->num < node2->num) {
+        res = node1;
+        node1 = SLIST_NEXT(node1, field);
+    }
+    else {
+        res = node2;
+        node2 = SLIST_NEXT(node2, field);
+    }
+
+    ptr = res;
+
+    while (node1 != NULL && node2 != NULL) {
+        if (node1->num < node2->num) {
+            SLIST_NEXT(ptr, field) = node1;
+            node1 = SLIST_NEXT(node1, field);
+        }
+        else {
+            SLIST_NEXT(ptr, field) = node2;
+            node2 = SLIST_NEXT(node2, field);
+        }
+        ptr = SLIST_NEXT(ptr, field);
+    }
+
+    if      (node1 != NULL) {SLIST_NEXT(ptr, field) = node1;}
+    else if (node2 != NULL) {SLIST_NEXT(ptr, field) = node2;}
+
+    return res;
+}
+
+/* 对链表节点进行归并排序 */
+static num_node *mergeSort(num_node *node)
+{
+    if (node==NULL || SLIST_NEXT(node,field)==NULL) { 
+        return node;
+    }
+
+    num_node *fast = node;
+    num_node *slow = node;
+    while (SLIST_NEXT(fast, field)!=NULL) {
+        if (SLIST_NEXT(SLIST_NEXT(fast,field), field) == NULL) {
+            break;
+        }
+        fast = SLIST_NEXT(SLIST_NEXT(fast,field), field);
+        slow = SLIST_NEXT(slow, field);
+    }
+
+    fast = slow;
+    slow = SLIST_NEXT(slow, field);
+    SLIST_NEXT(fast,field) = NULL;
+    fast = mergeSort(node);
+    slow = mergeSort(slow);
+    return merge(fast, slow);
+}
+
+/* 对链表头进行归并排序 */
+void num_mergeSort(num_head *head)
+{
+    SLIST_FIRST(head) = mergeSort(SLIST_FIRST(head));
+}
+
+int main(void)
+{
+    // /* 从堆空间申请链表表头并初始化 */
+    // num_head *p_head = malloc(sizeof(num_head));
+    // SLIST_INIT(p_head);
+
+    // /* 为链表添加10个0~100的随机数 */
+    // num_node *elm;
+    // for (int i=0; i<10; ++i) {
+    //     elm = malloc(sizeof(num_node));
+    //     elm->num = rand() % 100;
+    //     SLIST_INSERT_HEAD(p_head, elm, field);
+    // }
+
+    // /* 打印排序前的链表 */
+    // printf("Before sort:\n");
+    // SLIST_FOREACH (elm, p_head, field) {
+    //     printf("addr = %p, num = %d\n", elm, elm->num);
+    // }
+
+    // /* 归并排序函数 */
+    // num_mergeSort(p_head);
+
+    // /* 打印排序后的链表 */
+    // printf("After sort:\n");
+    // SLIST_FOREACH (elm, p_head, field) {
+    //     printf("addr = %p, num = %d\n", elm, elm->num);
+    // }
+
+    // /* 销毁链表释放空间 */
+    // while (!SLIST_EMPTY(p_head)) {
+    //     num_node *ptr = SLIST_FIRST(p_head);
+    //     SLIST_REMOVE_HEAD(p_head, field);
+    //     free(ptr);
+    // }
+    // free(p_head);
+
+    char send_buf[1024];
+    printf("%lu", strlen(send_buf));
+
     return 0;
- }
- void swap(int *x, int *y) {
-    int t;
- 
-    t = x;
-    x = y;
-    y = t;
 }
